ea5cb349d37821c557d3d71682176ff8a9568b35
patrickgdl <patrickgdlima@gmail.com>
2020-06-26 00:27:24 -0300

    chore(first): first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# FireStarter
+
+Firestarter is an Angular PWA powered by Firebase.
+
+## Features
+
+- Angular 9.x + Firebase
+- Installable PWA
+- OAuth and Email/Password Signup with Firebase
+- Drag & drop Kanban demo with Firestore
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCiyIuBAU3ZUKFMChJxeeR1LS3_oeeGoqA',
+    authDomain: 'angularhackathon.firebaseapp.com',
+    databaseURL: 'https://angularhackathon.firebaseio.com',
+    projectId: 'angularhackathon',
+    storageBucket: 'angularhackathon.appspot.com',
+    messagingSenderId: '838275467051',
+    appId: '1:838275467051:web:d9019509c66554e446a76e',
+    measurementId: 'G-SLRMR1PWSE',
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
